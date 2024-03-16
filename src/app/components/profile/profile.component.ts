082ee986f7c645129d45956512992b195cbab538
Angel <angelnthebe00@gmail.com--global user.email>
2024-03-16 18:47:57 +0200

    profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  constructor(public api: ApiService) {
+    const data = this.api.genericGet('/get-profile')
+      .subscribe({
+        next: (res: any) => {
+        const item = res;
+         console.log( item);
+        },
+        error: (err: any) => console.log('Error', err),
+        complete: () => { }
+      });
+  }
+  profiles: any[] = [
+    {
+      name: 'Angel',
+      surname: 'Nthebe',
+      address: '327 Mosiliki Jozi ',
+      id: '123456789',
+      dateOfBirth: new Date('1990-01-01'),
+      gender: 'Female',
+      policyNumber: 'ABC123',
+      email: 'angel@gmail.com',
+      contact: '1234567890'
+    }]
+    
 
 }
